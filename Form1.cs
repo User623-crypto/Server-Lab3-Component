e4fcc944d022543f93ad81d277dc4ebd4c48f641
User623-crypto <roel.nasufi@outlook.com>
2021-01-03 17:23:26 +0100

    Updated

--- Form1.cs
+++ Form1.cs
@@ -68,7 +68,6 @@ namespace Server
                                 string message = Encoding.UTF8.GetString(receivedBytes);
 
                                 llogjikoMesazhin(message, stream);
-                                //write(stream, Encoding.UTF8.GetBytes("Tekst" + new Random().Next(5,5084).ToString()));
                                 label1.BeginInvoke(new InvokeDelegate(InvokeMethod), message);
                             }
                             else Thread.Sleep(1); //Alternative Duhet një funksion "is-alive" -Kod i keq i pashkallëzueshëm
@@ -90,11 +89,14 @@ namespace Server
             {
 
                 byte[] buffer = new byte[1024];
-                stream.Read(buffer, 0, buffer.Length);
-                recivedBytes.AddRange(buffer);//Shtohet ne fund te listes
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                byte[] correctBuffer = new byte[bytesRead];
+                Array.Copy(buffer, correctBuffer, bytesRead);
+
+                recivedBytes.AddRange(correctBuffer);//Shtohet ne fund te listes
             }
 
-            recivedBytes.RemoveAll(b => b == 0); //Njëri nga mesazhet nuk do te përmbushi dot 1024 => pjesa e pa përmbushur do të hiqet
+            // recivedBytes.RemoveAll(b => b == 0); //Njëri nga mesazhet nuk do te përmbushi dot 1024 => pjesa e pa përmbushur do të hiqet
 
             return recivedBytes.ToArray();
         }
@@ -148,66 +150,4 @@ namespace Server
             }
         }
     }
-
-   
-
-    //Class to handle each client request separatly
-    /*public class handleClinet
-    {
-        TcpClient clientSocket;
-        string clNo;
-        public void startClient(TcpClient inClientSocket, string clineNo)
-        {
-            this.clientSocket = inClientSocket;
-            this.clNo = clineNo;
-            Thread ctThread = new Thread(doChat);
-            ctThread.Start();
-        }
-        private void doChat()
-        {
-            int requestCount = 0;
-            byte[] bytesFrom = new byte[10025];
-            string dataFromClient = null;
-            Byte[] sendBytes = null;
-            string serverResponse = null;
-            string rCount = null;
-            requestCount = 0;
-
-            while ((true))
-            {
-                try
-                {
-                    requestCount = requestCount + 1;
-                    NetworkStream networkStream = clientSocket.GetStream();
-                    if (networkStream.CanRead && networkStream.CanWrite)
-                    {
-                        networkStream.Read(bytesFrom, 0, bytesFrom.Length);
-                        dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom);
-                        dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"));
-                        Console.WriteLine(" >> " + "From client-" + clNo + dataFromClient);
-
-                        rCount = Convert.ToString(requestCount);
-                        serverResponse = "Server to clinet(" + clNo + ") " + rCount;
-                        sendBytes = Encoding.ASCII.GetBytes(serverResponse);
-                        networkStream.Write(sendBytes, 0, sendBytes.Length);
-                        networkStream.Flush();
-                        Console.WriteLine(" >> " + serverResponse);
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ekzekutohet kjo");
-                        clientSocket.Close();
-                        networkStream.Close();
-                        break;
-                    }
-                   
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine(" >> " + ex.ToString());
-                }
-            }
-        }
-    
-    }*/
 }
